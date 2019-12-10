c0981d8a22539b1c9eb3b7c98d99cd02d007e614
iggy-pigy <ilgakokorisa@gmail.com>
2019-12-10 21:51:49 +0000

    Added conditional statements and  comparisons to function

--- challenges/skiJump.js
+++ challenges/skiJump.js
@@ -5,8 +5,16 @@ function skiJump(mountain) {
     }
     var speed = height * 1.5;
     var jumpLenght = (height * speed * 9) / 10;
-    var roundedJumpLenght = Math.round(jumpLenght * 100) /100;
-    return roundedJumpLenght;
+    var roundedJumpLenght = Math.round(jumpLenght * 100) / 100;
+    if (roundedJumpLenght < 10) {
+        return `${roundedJumpLenght} metres: He's crap!`
+    } else if (roundedJumpLenght >= 10 && roundedJumpLenght < 25) {
+        return `${roundedJumpLenght} metres: He's ok!`
+    } else if (roundedJumpLenght >= 25 && roundedJumpLenght < 50) {
+        return `${roundedJumpLenght} metres: He's flying!`
+    } else {
+        return `${roundedJumpLenght} metres: Gold!!`
+    };
 }
 
-module.exports=skiJump;
+module.exports = skiJump;
