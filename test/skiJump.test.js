257d93af7ed62a4776da8d8f0cd88e92c93114c3
iggy-pigy <ilgakokorisa@gmail.com>
2019-12-10 22:02:02 +0000

    Changed skiJump.test

--- test/skiJump.test.js
+++ test/skiJump.test.js
@@ -3,7 +3,7 @@ const skiJump = require("../challenges/skiJump");
 describe.only("skiJump", () => {
   test("returns the lenght of the resulting jump", () => {
     const actual = skiJump(['*', '**', '***', '****', '*****', '******', '*******', '********']);
-    expect(actual).toBe(86.40);
+    expect(actual).toBe('86.40 metres: Gold!!');
   });
 
   test("returns the length of the resulting jump as a string in the following format", () => {
