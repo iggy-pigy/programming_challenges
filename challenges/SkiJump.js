8724f3e22925bebfe947350d20c198a7d3f69166
iggy-pigy <ilgakokorisa@gmail.com>
2019-12-10 21:12:59 +0000

    Added  skiJump function

---
+++ challenges/SkiJump.js
@@ -0,0 +1,12 @@
+function skiJump(mountain) {
+    var height = 0;
+    for (i = 0; i < mountain.length; i++) {
+        height += 1;
+    }
+    var speed = height * 1.5;
+    var jumpLenght = (height * speed * 9) / 10;
+    var roundedJumpLenght = jumpLenght.toFixed(2);
+}
+skiJump(['*', '**', '***', '****', '*****', '******']);
+
+module.exports=skiJump;
