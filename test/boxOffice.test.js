fb9a5a667bcf3f135aee44e450055330e6184dd1
iggy-pigy <ilgakokorisa@gmail.com>
2019-12-16 13:26:26 +0000

    Added one more test to boxOffice function

--- test/boxOffice.test.js
+++ test/boxOffice.test.js
@@ -3,6 +3,10 @@ const tickets = require("../challenges/boxOffice");
 describe("tickets", () => {
   test("returns YES, if clerck can sell a ticket to every person and give change with the bills he has at hand at that moment, otherwise returns NO.", () => {
     const actual = tickets([25, 100]);
+    expect(actual).toBe("NO");
+  });
+  test("returns YES, if clerck can sell a ticket to every person and give change with the bills he has at hand at that moment, otherwise returns NO.", () => {
+    const actual = tickets([25, 25, 50, 50]);
     expect(actual).toBe("YES");
   });
 });
