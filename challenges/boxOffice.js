3801abe685f17b394a2442d603ab2a8f28579ad6
iggy-pigy <ilgakokorisa@gmail.com>
2019-12-17 21:00:49 +0000

    Solved tickets function

--- challenges/boxOffice.js
+++ challenges/boxOffice.js
@@ -1,12 +1,37 @@
 function tickets(peopleInLine) {
-    const sum = peopleInLine.reduce((accumulator, currentVal) => {
-        if ((currentVal - 25) > accumulator) {
-            return "NO";
-        } else {
-            return accumulator + currentVal;
+    let amount25 = 0;
+    let amount50 = 0;
+    let amount100 = 0;
+
+    for (let i of peopleInLine) {
+
+        if (i === 100) {
+            if (amount25 >= 3) {
+                amount25 -= 3;
+                amount100++;
+            } else if (amount25 >= 1 && amount50 >= 1) {
+                amount25 -= 1;
+                amount50 -= 1;
+                amount100++;
+            } else {
+                return "NO";
+            }
         }
-    });
-    return sum;
+
+        if (i === 50) {
+            if (amount25 >= 1) {
+                amount25--;
+                amount50++;
+            } else {
+                return "NO";
+            }
+        }
+
+        if (i === 25) {
+            amount25++;
+        }
+    }
+    return "YES";
 }
 
 module.exports = tickets;
