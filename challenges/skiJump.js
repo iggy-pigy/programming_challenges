9f4fa1f76453020a7dbd056a87c23cf09791d408
iggy-pigy <ilgakokorisa@gmail.com>
2020-02-09 20:39:56 +0000

    Updated function skiJump

--- challenges/skiJump.js
+++ challenges/skiJump.js
@@ -1,14 +1,17 @@
 function skiJump(mountain) {
-    const jumpLenght = ((mountain.length * (mountain.length * 1.5) * 9) / 10).toFixed(2)
-    if (jumpLenght < 10) {
-        return `${jumpLenght} metres: He's crap!`
-    } else if (jumpLenght >= 10 && jumpLenght < 25) {
-        return `${jumpLenght} metres: He's ok!`
-    } else if (jumpLenght >= 25 && jumpLenght < 50) {
-        return `${jumpLenght} metres: He's flying!`
-    } else {
-        return `${jumpLenght} metres: Gold!!`
-    };
+  const jumpLength = (
+    (mountain.length * (mountain.length * 1.5) * 9) /
+    10
+  ).toFixed(2);
+  if (jumpLength < 10) {
+    return `${jumpLength} metres: He's crap!`;
+  } else if (jumpLength >= 10 && jumpLength < 25) {
+    return `${jumpLength} metres: He's ok!`;
+  } else if (jumpLength >= 25 && jumpLength < 50) {
+    return `${jumpLength} metres: He's flying!`;
+  } else {
+    return `${jumpLength} metres: Gold!!`;
+  }
 }
 
 module.exports = skiJump;
--- test/skiJump.test.js
+++ test/skiJump.test.js
@@ -1,13 +1,22 @@
 const skiJump = require("../challenges/skiJump");
 
 describe("skiJump", () => {
-  test("returns the lenght of the resulting jump", () => {
-    const actual = skiJump(['*', '**', '***', '****', '*****', '******', '*******', '********']);
-    expect(actual).toBe('86.40 metres: Gold!!');
+  test("returns the length of the resulting jump", () => {
+    const actual = skiJump([
+      "*",
+      "**",
+      "***",
+      "****",
+      "*****",
+      "******",
+      "*******",
+      "********"
+    ]);
+    expect(actual).toBe("86.40 metres: Gold!!");
   });
 
   test("returns the length of the resulting jump as a string in the following format", () => {
-    const actual = skiJump(['*', '**', '***', '****', '*****', '******']);
-    expect(actual).toBe('48.60 metres: He\'s flying!');
+    const actual = skiJump(["*", "**", "***", "****", "*****", "******"]);
+    expect(actual).toBe("48.60 metres: He's flying!");
   });
 });
