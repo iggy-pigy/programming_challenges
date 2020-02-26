164f30cf849dcff1a3d860e263719fe9028c7e56
iggy-pigy <ilgakokorisa@gmail.com>
2020-02-26 09:15:06 +0000

    Added test to growthOfPopulation .test.js file

--- test/growthOfPopulation.test.js
+++ test/growthOfPopulation.test.js
@@ -1,7 +1,10 @@
-const nbYear = require("./challenges/growthOfPopulation");
+const nbYear = require("../challenges/growthOfPopulation");
 
 describe("nbYear", () => {
   test("returns number of years (15) which are needed to reach the population of 5000 people, if starting population is 1500 and every year it grows by 5% and 100 people", () => {
     expect(nbYear(1500, 5, 100, 5000)).toBe(15);
+  });
+  test("returns number of years (10) which are needed to reach the population of 2000000 people, if starting population is 1500000 and every year it grows by 2.5% and 10000 people", () => {
+    expect(nbYear(1500000, 2.5, 10000, 2000000)).toBe(10);
   });
 });
